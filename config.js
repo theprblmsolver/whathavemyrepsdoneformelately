3506ab34c8584ef6d1ab9399ff9e591231d6ec92
theprblmsolver <github@things4locals.com>
2026-03-16 03:15:43 -0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - YOUR REAL KEYS GO HERE (local only)
 window.APP_CONFIG = {
     GOOGLE_API_KEY: "AIzaSyDnngVWPJMbfSvQBa8CFdQkwNohoQifbf4",
-    APIFY_TOKEN: "apify_api_F6Sf44jRfcJUjRwEbUw778jAfSfgT32GNQhY"
+    
 };
